72042e9221c33a58ace9db06ee198d4e61a98ca6
Tommi Saarikangas <tommi.saarikangas@gmail.com>
2020-05-27 16:18:34 +0200

    Implement logout button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import Login from './components/Login'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
@@ -15,6 +23,7 @@ const App = () => {
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
         <button onClick={() => setPage('login')}>login</button>
+        <button onClick={logout}>logout</button>
       </div>
 
       <Authors
